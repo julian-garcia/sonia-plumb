d4629c878ceac8f070d143649763f6f7552fd7c4
Julian Garcia <julian.garcia.leoni@gmail.com>
2023-09-16 16:35:37 +0100

    Setup vitejs + wordpress

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './templates/**/*.{html,php}',
+    './**/*.{html,php}',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
